122487d7985c7e327753a7042402aadb20fffc2e
Arakawa <lylaaaaaa@gmail.com>
2021-02-05 13:02:44 -0600

    Initial Commit

---
+++ Sample_TwitterTopScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample_TwitterTopScreen
+//
+//  Created by LylaArakawa on 05/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
